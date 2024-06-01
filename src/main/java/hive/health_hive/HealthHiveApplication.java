d365001166d21e482de0ec8a62b350ae72a460bf
Nikhil-ai-dev <nikhil.nn245@gmail.com>
2024-06-01 20:01:13 +0530

    -Adding initial springboot files

---
+++ src/main/java/hive/health_hive/HealthHiveApplication.java
@@ -0,0 +1,13 @@
+package hive.health_hive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthHiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthHiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hive/health_hive/HealthHiveApplicationTests.java
@@ -0,0 +1,13 @@
+package hive.health_hive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthHiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
